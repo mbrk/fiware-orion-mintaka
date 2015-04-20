64f1247155e00edf3bbcf702d7ca69583ccee025
Malte Burkhardt <info@bitsundpixel.de>
2015-04-20 22:44:40 +0200

    added lots of functions, replaced repeating code with function calls

--- index.js
+++ index.js
@@ -44,15 +44,7 @@ module.exports = {
 	 */
 	requestEntities: function(){
 		var url = brokerUrl + 'contextEntities';
-		return new promise(function(resolve, reject){
-			rest.get(url, options)
-					.on('success', function(data, response) {
-						resolve(data);
-					})
-					.on('fail', function(data, response) {
-						reject(data);
-					});
-		});
+		return this.createPromiseForGET(url, options);
 	},
 
 	/**
@@ -80,17 +72,38 @@ module.exports = {
 	 */
 	registerEntity: function(entity, json){
 		var url = brokerUrl + 'contextEntities/' + entity;
-		console.log('registercontext', url);
-		return new promise(function(resolve, reject){
-			rest.postJson(url, json, options)
-					.on('success', function(data, response) {
-						resolve(data);
-					})
-					.on('fail', function(data, response) {
-						reject(data);
-					});
-		});
+		return this.createPromiseForJsonPOST(url, json, options);
 	},
+
+	/**
+	 * register entity with specified type
+	 * @param entity
+	 * @param type
+	 * @param json
+	 * @returns {*}
+	 */
+	registerEntityWithType: function(entity, type, json){
+		var url = brokerUrl + 'contextEntities/type/' + type + "/id/" + entity;
+		return this.createPromiseForJsonPOST(url, json, options);
+	},
+
+	/**
+	 * set attribute on entity
+	 *
+	 * @param entity
+	 * @param attribute
+	 * @param value
+	 * @returns {*}
+	 */
+	registerAttribute: function(entity, attribute, value){
+		var url = brokerUrl + 'contextEntities/' + entity + '/attributes/' + attribute;
+		var json = {
+			"value" : "" + value
+		};
+		return this.createPromiseForJsonPOST(url, json, options);
+	},
+
+
 
 	/**
 	 * delete a registered entity
@@ -110,5 +123,86 @@ module.exports = {
 					});
 		});
 
+	},
+
+	/**
+	 * get all attributes from specified entity
+	 *
+	 * @param entity
+	 * @returns {*}
+	 */
+	queryAttributes: function(entity){
+		var url = brokerUrl + 'contextEntities/' + entity;
+		return this.createPromiseForGET(url, options);
+	},
+
+	/**
+	 * get specified attribute from specified entity
+	 * @param entity
+	 * @param attribute
+	 * @returns {*}
+	 */
+	querySingleAttribute: function(entity, attribute){
+		var url = brokerUrl + 'contextEntities/' + entity + '/attributes/' + attribute;
+		return this.createPromiseForGET(url, options);
+	},
+
+	/**
+	 * get all entities from specified type
+	 *
+	 * @param type
+	 * @returns {*}
+	 */
+	queryEntitiesByType: function(type){
+		var url = brokerUrl + 'contextEntityTypes/' + type;
+		return this.createPromiseForGET(url, options);
+	},
+
+	/**
+	 * get a specified attribute from all entities with specified type
+	 * @param type
+	 * @param attribute
+	 * @returns {*}
+	 */
+	querySingleAttributeByEntityType: function(type, attribute){
+		var url = brokerUrl + 'contextEntityTypes/' + type + '/attributes/' + attribute;
+		return this.createPromiseForGET(url, options);
+	},
+
+	/**
+	 * helper to create promises for 'restler' GET calls
+	 * @param url
+	 * @param options
+	 * @returns {*}
+	 */
+	createPromiseForGET: function(url, options){
+		return new promise(function(resolve, reject){
+			rest.get(url, options)
+					.on('success', function(data, response) {
+						resolve(data);
+					})
+					.on('fail', function(data, response) {
+						reject(data);
+					});
+		})
+	},
+
+	/**
+	 * helper to create promises for 'restler' POST calls
+	 * @param url
+	 * @param json
+	 * @param options
+	 * @returns {*}
+	 */
+	createPromiseForJsonPOST: function(url, json, options){
+		return new promise(function(resolve, reject){
+			rest.postJson(url, json, options)
+					.on('success', function(data, response) {
+						resolve(data);
+					})
+					.on('fail', function(data, response) {
+						reject(data);
+					});
+		});
 	}
 };
